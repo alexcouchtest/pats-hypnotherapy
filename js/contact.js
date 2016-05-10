2af7820481378f96924b6f0e68c88dcf8bf129ab
Alex <tempus.studios.contact@gmail.com>
2016-05-09 21:33:26 -0700

    Add files via upload

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
-	var contactlinks = $(".contact-links");
-	var contactinfo = $(".contact-info");
-	var emaillink = $(".email-link");
-	var email = $(".email");
-	var numberlink = $(".number-link");
-	var number = $(".number");
-	contactinfo.hide();
-	email.hide();
-	number.hide();
+	var contactlinks = ".contact-links";
+	var contactinfo = ".contact-info";
+	var emaillink = ".email-link";
+	var email = ".email";
+	var numberlink = ".number-link";
+	var number = ".number";
+	var apptlink = ".email-link-apntmnt";
+	var appt = ".appt";
+	var infolink = ".email-link-info";
+	var info = ".info";
+	$(contactinfo).hide();
+	$(email).hide();
+	$(number).hide();
+	$(appt).hide();
+	$(info).hide();
 	$(contactlinks).click(function(){
 		$(contactinfo).toggle(300);
 	});
@@ -16,5 +22,11 @@ $(document).ready(function(){
 	});
 	$(numberlink).click(function(){
 		$(number).toggle(300);
+	});
+	$(apptlink).click(function(){
+		$(appt).toggle(300);
+	});
+	$(infolink).click(function(){
+		$(info).toggle(300);
 	});
 });
